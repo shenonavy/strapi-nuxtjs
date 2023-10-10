fbc580014f7ad998ffebd140a0485fb74a304dba
Shafran Ashraff <shafran@bistecglobal.com>
2023-10-10 13:24:54 +0530

    clean

--- react-app/pages/_app.tsx
+++ react-app/pages/_app.tsx
@@ -1,20 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const navigate = useRouter();
-
-  const pathName =
-    navigate.pathname === "/" ? "Home" : navigate.pathname.substring(1);
-
-  return (
-    <>
-      <Head>
-        
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
